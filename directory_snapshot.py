c13f89a00d7a61adeb62a17f5a42bdd6334ade48
Christian Häfeli <christian.haefeli@gmx.ch>
2024-10-02 22:52:21 +0200

    added DirectorySnapshot.diff_snapshot()

--- directory_snapshot.py
+++ directory_snapshot.py
@@ -1,3 +1,5 @@
+import copy
+import glob
 import json
 import os
 from pathlib import Path
@@ -10,52 +12,112 @@ class DirectorySnapshot:
 
     def __init__(self, rootDir:Path):
         self._rootdir = rootDir
-        self.create_snapshot(self._rootdir)
-        self.write_json_snapshot()
+        self._snapshot = {"elements":[]}
 
     @property
     def snapshot(self):
-        return self._snapshot
+        return self._snapshot 
+    
+    def element_found(self, path_as_key: str):
+        found = False
+        for el in self._snapshot["elements"]:
+           if el["path"] == path_as_key:
+               found = True
+               break
+        return found
 
-    def create_snapshot(self, rootdir:Path, overwrite=False, only_one_dir=False) -> tuple:
-        start_time = time.time()   
-        self._snapshot = {}
+    def get_element_list(self, file_or_dir="ALL"):
+        retlist = self._snapshot["elements"]
+        if file_or_dir != "ALL":
+            retlist = [el for el in self._snapshot["elements"] if el["type"] == file_or_dir ]
+        return retlist 
+
+    def create_snapshot(self, overwrite=False, only_one_dir=False) -> tuple:
+        start_time = time.time()           
         totalbytes = 0  
-        for dir, subdirs, files in os.walk(rootdir):
+        for dir, subdirs, files in os.walk(self._rootdir):
             dir_path = Path(dir)
-            self._snapshot[dir_path] = [len(files), "DIR"]
+            self._snapshot["elements"].append(
+                {"type" : "DIR", 
+                 "path" : dir_path.as_posix(), 
+                 "nrof_files" : len(files)
+                 }) 
             
             for filename in files:
-                if filename.startswith("."):  #don't put it in the snapshot 
+                if filename.startswith(".md5_hashes"):  #don't put it in the snapshot 
                     continue
                 file_path = dir_path / filename
-                file_size = file_path.stat().st_size
-                totalbytes = totalbytes + file_size
-                self._snapshot[file_path] = [file_size, "FILE"]
+                file_size = file_path.stat().st_size                               
+                self._snapshot["elements"].append(
+                    {"type" : "FILE", 
+                    "path" : file_path.as_posix(), 
+                    "filelen" : len(files)
+                    }) 
 
+                totalbytes = totalbytes + file_size
             if only_one_dir: 
-                break  # not walking down the tree ... just snapsthot from the rootdir.
+                break  # stop walking down the tree ... just snapshot the rootdir.
 
         runtime = time.time() - start_time
         return (runtime, totalbytes)
+ 
+    def load_snapshot(self, number:int):
+        search_pattern = os.path.join(DirectorySnapshot.SNAPSHOT_HYSTORY_DIR, f"{number:04d}-*-VS.json")
+        matching_files = glob.glob(search_pattern)
+        if matching_files: 
+            self.read_json_snapshot_file(matching_files[0])
     
-    def get_last_snapshot_number(self):
-        history_list = self.get_snapshot_history_file_list()
-        biggest_nr = 0
-        for fn in history_list:
-            nr = int (fn[0:5])
-            if nr > biggest_nr:
-                biggest_nr = nr
-            print(fn)
-        return biggest_nr 
+    def load_last_snapshot(self):
+        file_path = DirectorySnapshot.SNAPSHOT_HYSTORY_DIR / self.get_last_snapshot_filename() 
+        self.read_json_snapshot_file(file_path)
 
     def get_snapshot_history_file_list(self):
         return [p.name for p in Path(DirectorySnapshot.SNAPSHOT_HYSTORY_DIR).iterdir() if p.is_file()]
     
+    def diff_snapshot(self, older_snapshot: 'DirectorySnapshot'):
+        diff_list = {}
+        deep_copied_older_snapshot = copy.deepcopy(older_snapshot.get_element_list())  
+     
+        for new_el in self.get_element_list():            
+            path = new_el["path"]
+            if not older_snapshot.element_found(path):
+                diff_list[path] = "+"
+            else:
+                deep_copied_older_snapshot.remove(new_el)
+        # see if there are elements in the older_snapshot which are not in the new one:
+        for old_el in deep_copied_older_snapshot:
+            path = old_el["path"]
+            diff_list[path] = "-"
+
+        return diff_list
+    
+
+    def get_last_snapshot_number(self):
+        history_list = self.get_snapshot_history_file_list()
+        biggest_nr = 0
+        for fn in history_list:
+            nr = int (fn[0:4])
+            if nr > biggest_nr:
+                biggest_nr = nr           
+        return biggest_nr    
+    
+    def get_last_snapshot_filename(self):
+        highest_number = -1
+        last_snapshot_filename = None
+        
+        for filename in os.listdir(DirectorySnapshot.SNAPSHOT_HYSTORY_DIR):
+            # Check if the filename matches the pattern 'xxxx-JJJJMMDD-VS.json'
+            if filename.endswith('-VS.json') and len(filename) == 21 and filename[:4].isdigit():            
+                file_number = int(filename[:4])
+                if file_number > highest_number:
+                    highest_number = file_number
+                    last_snapshot_filename = filename
+        return last_snapshot_filename      
+    
     def get_snapshot_filename(self):        
-        strdate = f"{datetime.now().year}{datetime.now().month}{datetime.now().day}"
+        strdate = f"{datetime.now().year}{datetime.now().month:02d}{datetime.now().day:02d}"
         next_snapshot_nr = self.get_last_snapshot_number() + 1
-        return f"{next_snapshot_nr : 05d}-{strdate}-VS.json"
+        return f"{next_snapshot_nr:04d}-{strdate}-VS.json"
     
     def write_snapshot(self):
         self._snapshot_filename = DirectorySnapshot.SNAPSHOT_HYSTORY_DIR / self.get_snapshot_filename()
@@ -72,16 +134,41 @@ class DirectorySnapshot:
         with open(self._snapshot_filename, "w", encoding='utf-8') as f:
             json.dump(self.snapshot, f, ensure_ascii=False, indent=4)
 
+    def read_json_snapshot_file(self, file_path: Path):        
+        with open(file_path, "r",  encoding='utf-8') as fn:
+            self._snapshot = json.load(fn)
 
-def main():
-    print("main(): start ...")
+def create_snapshot():
     src = r"C:\tmp\testsrc"
     snapshot = DirectorySnapshot(src)
-    
-    # print(snapshot.get_last_snapshot_number())
-    # print(snapshot.get_snapshot_filename())
-    
-    print(snapshot.snapshot)
+    snapshot.create_snapshot()
+    snapshot.write_json_snapshot()
+
+def print_snapshots():
+    src = r"C:\tmp\testsrc"
+    last_snapshot = DirectorySnapshot(src)
+    last_snapshot.load_last_snapshot()
+    second_snapshot = DirectorySnapshot(src)
+    second_snapshot.load_snapshot(2)
+
+    diff_list = last_snapshot.diff_snapshot(second_snapshot)
+    for key, value in diff_list.items():
+        print(f"{value} ¦ {key}")
+
+    # for el in last_snapshot.get_element_list():
+    #     print(el)
+
+    # found = last_snapshot.element_found('C:/tmp/testsrc/unicode_validation/__pycache__/continuationbyte.cpython-312.pyc')
+    # print(f"{found=}")
+
+    # for el in second_snapshot.get_element_list():
+    #     print(el)
+
+def main():
+    print("main(): start ...")    
+    # create_snapshot()
+    print_snapshots()
+
     #runtime = helper.create_md5hashes_for_subdirs(src, overwrite=True)
     # print(f"Runtime: --- {runtime:.3f} seconds.  Bytes={bytes}")  
     print("main(): all done")
